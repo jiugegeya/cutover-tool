b17eb0e5222d2e25c1b040978bf027d25394bd5c
jiugegeya <15008493442@139.com>
2026-05-29 17:34:36 +0800

    Add project configuration files

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+})
